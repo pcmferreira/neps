8329c900340ed04dc633b19c333683a98442f65c
Pedro César Mesquita Ferreira <pedrocmferreira@gmail.com>
2021-11-27 02:57:57 -0300

    1532.cpp

---
+++ solutions/1532.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double x; cin >> x;
+    cout <<
+        fixed << setprecision(2) <<
+        "Joao e Henrique devem guardar R$ " << x * 1.7 << endl;
+    return 0;
+}
